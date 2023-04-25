b33d7db26174d1ecef67b3a58e9d2ab2c7bca593
SimoneCastellan <simo.castellan@gmail.com>
2023-04-25 11:51:24 +0100

    Definition red system

--- src/PBW.lean
+++ src/PBW.lean
@@ -1,1 +1,13 @@
+import algebra.free_monoid.basic
+import algebra.free_algebra
+
+
 --We need to prove the Diamond Lemma!
+
+variables (X : Type) 
+variables (R: Type) [comm_ring R]
+
+
+def reduction_system : Type := set (free_monoid X × free_algebra R X)
+
+
