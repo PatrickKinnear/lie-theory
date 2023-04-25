8d2924456ad65ed194453f80dcd4b62e72dab4d9
Jan Pulmann <jan.pulmann@gmail.com>
2023-04-25 14:02:30 +0100

    reduction system

--- src/PBW.lean
+++ src/PBW.lean
@@ -1,5 +1,6 @@
 import algebra.free_monoid.basic
 import algebra.free_algebra
+open set
 
 
 --We need to prove the Diamond Lemma!
@@ -7,7 +8,9 @@ import algebra.free_algebra
 variables (X : Type) 
 variables (R: Type) [comm_ring R]
 
+structure reduction_system :=
+(σ : set (free_monoid X × free_algebra R X))
+(nondegeneracy: ∀ p : σ, free_monoid.lift (free_algebra.ι R) p.val.1 ≠ p.val.2 )
 
-def reduction_system : Type := set (free_monoid X × free_algebra R X)
 
 
