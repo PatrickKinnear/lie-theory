eca833907ec9121b573f14491a3e2cbfdf04e6ce
Jan Pulmann <jan.pulmann@gmail.com>
2023-04-25 15:42:19 +0100

    reduction defined

--- src/PBW.lean
+++ src/PBW.lean
@@ -1,6 +1,8 @@
 import algebra.free_monoid.basic
 import algebra.free_algebra
 import algebra.module.submodule.basic
+import linear_algebra.free_algebra
+import linear_algebra.basis
 open set
 
 
@@ -19,9 +21,8 @@ variable (S : reduction_system X R)
 
 def inc_free_monoid_free_alg : free_monoid X →* free_algebra R X:= free_monoid.lift (free_algebra.ι R)
 
-def reduction (σ : S.set) (A B: free_monoid X) : free_algebra R X → free_algebra R X := sorry
 
-noncomputable def reduction_fake (σ : S.set) (A : free_monoid X) (B : free_monoid X) : free_monoid X → free_algebra R X := 
+noncomputable def reduction_on_basis (σ : S.set) (A B : free_monoid X) : free_monoid X → free_algebra R X := 
 begin
   intro x,
   by_cases x = A*σ.val.1*B,
@@ -33,27 +34,9 @@ begin
   },
 end
 
+def randomname : free_monoid X → free_algebra R X := sorry
 
-
-
-
-noncomputable def reduction_step1 : free_monoid X → free_monoid X → free_monoid X × free_algebra R X →  free_monoid X → free_algebra R X :=
-begin
-  intros A B σ x,
-  by_cases x= A*σ.1*B,
-  {
-    exact (inc_free_monoid_free_alg X R A)*σ.2*(inc_free_monoid_free_alg X R B), 
-  },
-  {
-    exact (inc_free_monoid_free_alg X R x),
-  },
-end
-
-def reduction : free_monoid X → free_monoid X → free_monoid X × free_algebra R X →  free_algebra R X →  free_algebra R X :=
-begin
-  intros A B σ,
-  
-end
+noncomputable def reduction (σ : S.set) (A B: free_monoid X) : free_algebra R X →ₗ[R] free_algebra R X := basis.constr (free_algebra.basis_free_monoid R X) R (reduction_on_basis X R S σ A B) 
 
 
 def irr_set : set (free_algebra R X) := { a : free_algebra R X | ∀ σ : S.set, ∀ A : free_monoid X, ∀ B : free_monoid X, reduction X R S σ A B a ≠ a}
